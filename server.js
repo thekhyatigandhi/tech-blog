82cab1b4c027c6706f95ff89b8f56f42751ce3e0
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-06-06 21:04:07 -0400

    uodated code in server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const sequelize = require("./config/connection");
+const path = require("path");
+const routes = require("./controllers");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const helpers = require("./utils/helpers");
+
+// import db connection
+const sequelize = require("./config/connection");

e80f70ed1f9b9eb49a4f52f9a5d73b376f461005
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-06-06 22:23:24 -0400

    modified seed file in seeds

---
+++ seeds/seed.js
@@ -0,0 +1,4 @@
+const sequelize = require("../config/connection");
+const seedUsers = require("./userData");
+const seedPosts = require("./postData");
+const seedComments = require("./commentData");

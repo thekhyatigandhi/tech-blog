c72ef8c7a5fe43371a032648391a8d87fd767154
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-06-09 16:29:22 -0400

    added code in home route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -37,16 +37,37 @@ router.get("/", async (req, res) => {
 
 router.get("/dash", async (req, res) => {
   try {
-    if (req.session.logged_in){
+    if (req.session.logged_in) {
       const postData = await Post.findAll({
-        where:{
-          user_id:req.session.user_id,
+        where: {
+          user_id: req.session.user_id,
         },
-        include[{
-          model: User
-        }]
-      })
-      }
+        include: [
+          {
+            model: User,
+            attribute: { exclude: ["passqord"] },
+          },
+          {
+            model: Comment,
+            include: {
+              model: User,
+              attributes: ["id", "username"],
+            },
+          },
+        ],
+        order: [["updated_at", "DESC"]],
+      });
+
+      const posts = postData.map((post) => post.get({ plain: true }));
+
+      res.render("dash", {
+        posts,
+        loggedIn: req.session.logged_in,
+        userId: req.session.user_id,
+      });
+      return;
+    } else {
+      res.redirect("/login");
     }
   } catch (err) {
     console.log(err);
@@ -62,11 +83,19 @@ router.get("/", async (req, res) => {
 });
 
 // render new post page (if logged in)
-router.get("/", async (req, res) => {
+router.get("/login", async (req, res) => {
   try {
+    if (req.session.logged_in) {
+      res.redirect("/");
+      return;
+    }
+    res.render("login");
   } catch (err) {
     console.log(err);
+    res.status(500).json(err);
   }
 });
+
+// render new post page (only if logged in)
 
 module.exports = router;

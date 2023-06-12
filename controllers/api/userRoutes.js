0dab3706a4a113dc451b690541565bd2af0c7d9b
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-06-12 14:26:54 -0400

    req session save on log in

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,21 +3,41 @@ const { User } = require("../../models");
 
 // ROUTES http://localhost:3001/api/user/
 
-// log-in 
-router.post('/login', async (req, res) => {
-    try {
-        const userData = await User.findOne({ where: { username: req.body.username }});
+// log-in
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { username: req.body.username },
+    });
 
-        if(!userData) {
-            res.status(400).json({ message: "We're sorry, but the login information you provided is not valid. Please double-check your username and password and try again." });
-            return;
-        }
-        const validatePassword = await userData.checkPassword(req.body.password);
+    if (!userData) {
+      res
+        .status(400)
+        .json({
+          message:
+            "We're sorry, but the login information you provided is not valid. Please double-check your username and password and try again.",
+        });
+      return;
+    }
+    const validatePassword = await userData.checkPassword(req.body.password);
 
+    if (!validatePassword) {
+      res
+        .status(400)
+        .json({
+          message:
+            "We're sorry, but the login information you provided is not valid. Please double-check your username and password and try again.",
+        });
+      return;
+    }
 
-        if(!validatePassword) {
-            res.status(400).json({ message: "We're sorry, but the login information you provided is not valid. Please double-check your username and password and try again." });
-            return;
-        }
-
-        
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      req.session.username = userData.username;
+      res.json({ user: userData, message: "You are logged in! Welcome!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});

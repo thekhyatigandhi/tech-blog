35ea96ce4a957c76c2901b6104c9f1417d343099
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-06-12 16:39:46 -0400

    added code to new post.js

---
+++ public/js/newPost.js
@@ -0,0 +1,6 @@
+async function newPost(event) {
+  event.preventDefault();
+
+  const title = document.querySelector("#new-post-title").value.trim();
+  const post_body = document.querySelector("#new-post-body").value.trim();
+}

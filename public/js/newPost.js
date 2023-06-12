8151fa34669ef4491a9af976c82ec664ead092f5
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-06-12 16:46:36 -0400

    added if statment for the new post

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -3,4 +3,16 @@ async function newPost(event) {
 
   const title = document.querySelector("#new-post-title").value.trim();
   const post_body = document.querySelector("#new-post-body").value.trim();
+
+  if (title && post_body) {
+    const response = await fetch(`/api/post`, {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            post_body,
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
 }

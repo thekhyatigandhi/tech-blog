07bb55bb49921c0982193a4b67648c66112b5f99
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-06-12 14:35:11 -0400

    setting up sign up and log out

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -11,23 +11,17 @@ router.post("/login", async (req, res) => {
     });
 
     if (!userData) {
-      res
-        .status(400)
-        .json({
-          message:
-            "We're sorry, but the login information you provided is not valid. Please double-check your username and password and try again.",
-        });
+      res.status(400).json({
+        message: "Incorrect email or password, please try again",
+      });
       return;
     }
     const validatePassword = await userData.checkPassword(req.body.password);
 
     if (!validatePassword) {
-      res
-        .status(400)
-        .json({
-          message:
-            "We're sorry, but the login information you provided is not valid. Please double-check your username and password and try again.",
-        });
+      res.status(400).json({
+        message: "Incorrect email or password, please try again",
+      });
       return;
     }
 
@@ -35,9 +29,38 @@ router.post("/login", async (req, res) => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
       req.session.username = userData.username;
-      res.json({ user: userData, message: "You are logged in! Welcome!" });
+      res.json({ user: userData, message: "You are now logged in! Welcome!" });
     });
   } catch (err) {
     res.status(400).json(err);
   }
 });
+
+// sign up
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      req.session.username = userData.username;
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// log out
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;

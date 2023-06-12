b24127547c3640233a6bbe2ba33a5601403bc15a
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-06-12 13:25:40 -0400

    created folders for API routes

---
+++ controllers/api/index.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const postRoutes = require("./postRoutes");
+const userRoutes = require("./userRoutes");
+const commentRoutes = require("./commentRoutes");

6a5a62f9a5d4f1918e62d1933dd95e5db41f1076
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-06-12 13:29:45 -0400

    added delete route to comments

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -17,3 +17,23 @@ router.post("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// delete comment
+router.delete("/:id", async (req, res) => {
+  try {
+    const comment = await Comment.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!comment) {
+      res.status(404).json({ message: "No comment found with that id!" });
+      return;
+    }
+    res.status(200).json(comment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
